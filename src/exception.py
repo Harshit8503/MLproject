511458e1b21035b2de2c6e5bcd1ccdf53377e29c
Harshit <harshitjain5670@gmail.com>
2024-07-26 16:12:13 +0530

    Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys #used to manipulate python runtime environment
-
+from src.logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() #exc_tb will contain all info about exception occured like at what line it has occured
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -11,7 +11,7 @@ def error_message_detail(error,error_detail:sys):
 class CustomException(Exception):
     #constructor
     def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
